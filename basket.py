c20c0d18727d5e3ef84559957e0bb0ea3967f70a
tiagoow <42955314+tiagoow@users.noreply.github.com>
2020-08-02 22:11:18 -0400

    fixed the error

--- basket.py
+++ basket.py
@@ -1,31 +1,21 @@
-DEFAULT_INITIAL_BASKET = ["orange", "apple"]
-
-def create_picnic_basket(healthy, hungry, initial_basket=DEFAULT_INITIAL_BASKET):
-
+def create_picnic_basket(healthy, hungry, initial_basket=None):
+    if initial_basket == None:
+        initial_basket = ["orange", "apple"]
     basket = initial_basket
-
     if healthy:
-
         basket.append("strawberry")
-
     else:
-
         basket.append("jam")
-
-
-
+    
     if hungry:
-
         basket.append("sandwich")
-
     return basket
 
 # Reproducer
-
 print("First basket:", create_picnic_basket(True, False))
 
 print("Second basket:", create_picnic_basket(False, True, ["tea"]))
 
 print("Third basket:", create_picnic_basket(True, True))
 
-print("WHY DO I HAVE 2 STRAWBERRIES WHEN I AM HEALTH AND HUNGRY?")
+#print("WHY DO I HAVE 2 STRAWBERRIES WHEN I AM HEALTH AND HUNGRY?")
